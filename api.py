087aeddf5bc179f647c81482e643be221135ec1a
Arx <83051653+Archs7331@users.noreply.github.com>
2023-01-09 22:20:20 +1100

    Update api.py

--- api.py
+++ api.py
@@ -6,7 +6,6 @@ import time
 from urllib.parse import urlparse
 from flask import Flask, send_file, request, jsonify
 
-# Open the config file and read the contents
 with open('attacks.yml', 'r') as f:
     config = yaml.safe_load(f)
 
@@ -23,7 +22,6 @@ maxtime=settings['config']['maxtime']
 #apikeys = config_toml.get('config').get('keys')
 #maxtime = int(config_toml.get('config').get('maxtime'))
 
-# Get the list of methods from the config file
 methods = config['methods']
 
 app = Flask(__name__)
@@ -34,23 +32,18 @@ def index():
 
 @app.route("/attack")
 def attack():
-    # Get the host argument from the request
     host = request.args.get('host')
 
-    # Check if the host argument is a valid IP address or URL
     is_valid_ip = False
     is_valid_url = False
     try:
-        # Check if the host argument is a valid IP address
         ip = ipaddress.ip_address(host)
         is_valid_ip = True
     except ValueError:
-        # Check if the host argument is a valid URL
         url = urlparse(host)
         if url.scheme and url.netloc:
             is_valid_url = True
 
-    # If the host argument is not a valid IP address or URL, return an error
     if not (is_valid_ip or is_valid_url):
         return jsonify(
             error=True,
@@ -86,15 +79,10 @@ def attack():
     else:
         with open('attacks.log', 'a') as f:
             f.write(f"{key} - {host}:{port} - {duration}s - {method}\n")
-        # Check if the specified method is in the list of methods from the config file
         if method in methods:
-            # Get the list of URLs for the specified method from the config file
             urls = methods[method]
 
-            # Start the timer
             start_time = time.perf_counter()
-
-            # Iterate over the list of URLs and send a request to each URL
             for url in urls:
                 url = url.replace('<<host>>', host).replace('<<host>>', host).replace('<<port>>', str(port)).replace('<<time>>', str(duration))
                 ua = {'User-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36'}
@@ -106,10 +94,8 @@ def attack():
                 else:
                     col = "\u001b[41m"
                 print(f"{col}{z}\u001b[0m - {url} - Request sent to API.")
-            # Stop the timer
             end_time = time.perf_counter()
 
-            # Compute the elapsed time
             elapsed_time = end_time - start_time
             elapsed_time =str(elapsed_time).split(".",1)[0]
 
